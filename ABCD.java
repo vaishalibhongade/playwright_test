d7ca00b6a518f96e0b72420058046736810c3179
vaishalibhongade <vaishalibhongade6@gmail.com>
2024-08-14 15:48:56 +0530

    java

---
+++ ABCD.java
@@ -0,0 +1,15 @@
+class Abcd
+{
+public static void main(String args[])
+{
+char n='E';
+for (char i='A';i<=n;i++)
+{
+for (char j='A';j<=i;j++)
+{
+System.out.print(i);
+}
+System.out.println( );
+}
+}
+}
